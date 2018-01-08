811b390d77d8617d8a8f4126489af028d2e1b214
Star Lee <zabra15@yahoo.com>
2018-01-08 16:55:09 -0500

    got <Card /> working in App.js

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Header from "./components/Header";
 import GameBoard from "./components/GameBoard";
+import Card from "./components/Card";
 
 class App extends React.Component {
     render () {
        return (
             <div>
-                <Header/>
-                <GameBoard/>
+                <Header />
+                <GameBoard />
+                <Card />
             </div>
        )
     }
---
+++ clicky/src/components/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Card extends React.Component {
+    render() {
+        return (
+            <span className="game-card">***</span>
+        )
+    }
+}
+
+export default Card;
