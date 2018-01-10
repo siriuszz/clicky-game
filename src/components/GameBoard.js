ad40afe11e6be8d3b8510dc51add53f163e020ea
Star Lee <zabra15@yahoo.com>
2018-01-10 14:48:33 -0500

    removed images

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -2,12 +2,8 @@ import React from 'react';
 // import { Cards } from './Cards';
 
 class GameBoard extends React.Component {
-    render () {
-        return (
-            <div>
-                <h1>This is the gameboard.</h1>
-                {/*<Cards />*/}
-            </div>
+    constructor(props) {
+        super(props);
         )
     }
 };
